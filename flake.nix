8eaf43a357e26e7ebe70a96322b0d386e5cf2039
rotaerk <m.scott.stewart@gmail.com>
2022-05-15 00:07:07 -0400

    Setup the sandbox project along with godot-haskell.

--- flake.nix
+++ flake.nix
@@ -8,14 +8,15 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = import nixpkgs { system = "x86_64-linux"; };
-    hs = pkgs.haskellPackages;
-    godotHaskellTest = hs.callCabal2nix "godotHaskellTest" ./. {};
+    hs = pkgs.haskell.packages.ghc8107;
+#    godotHaskellTest = hs.callCabal2nix "godotHaskellTest" ./. {};
   in
   {
-    packages.x86_64-linux = {};
+    packages.x86_64-linux = { inherit pkgs; };
 
     devShells.x86_64-linux.default = hs.shellFor {
-      packages = p: [ godotHaskellTest ];
+#      packages = p: [ godotHaskellTest ];
+      packages = p: [];
       nativeBuildInputs = [
         hs.cabal-install
         hs.haskell-language-server
--- sandbox/CHANGELOG.md
+++ sandbox/CHANGELOG.md
@@ -1,4 +1,4 @@
-# Revision history for godotHaskellTest
+# Revision history for sandbox
 
 ## 0.1.0.0 -- YYYY-mm-dd
 
